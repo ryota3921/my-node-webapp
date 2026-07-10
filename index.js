ff4da5140944741f785b99a997fd9611858fbd6d
ryota3921 <251703586+ryota3921@users.noreply.github.com>
2026-07-10 15:45:58 +0900

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
   res.end(
     "<h1>Azure App Service Demo</h1>" +
-    "<p>GitHub Actions から自動デプロイしています。ryota</p>"
+    "<p>GitHub Actions から自動デプロイしています。更新テスト 2026/7/10 ryota</p>"
   );
 });
 
